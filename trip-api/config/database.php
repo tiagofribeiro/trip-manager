911e26ebc92da80eb366978557dfae7e333ad7d4
Tiago Ribeiro <tg.frnds@outlook.com>
2025-07-08 05:57:46 -0300

    :gear: laravel project

---
+++ trip-api/config/database.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Str;
+
+return [
+
+    'default' => env('DB_CONNECTION', 'sqlite'),
+
+    'connections' => [
+
+        'sqlite' => [
+            'driver' => 'sqlite',
+            'url' => env('DB_URL'),
+            'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            'prefix' => '',
+            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
+            'busy_timeout' => null,
+            'journal_mode' => null,
+            'synchronous' => null,
+        ],
+
+    ],
+
+    'migrations' => [
+        'table' => 'migrations',
+        'update_date_on_publish' => true,
+    ],
+
+];
