ad826b9385c9894a002a12e3a9a2d32cfb58bade
Tiago Ribeiro <tg.frnds@outlook.com>
2025-07-09 03:50:29 -0300

    Auth (#1)
    * :gear: laravel project
    
    * :broom: cleanup & update
    
    * :star: login, register & JWT

---
+++ trip-api/app/Http/Controllers/AuthController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function profile(Request $request)
+    {
+        return response()->json($request->user());
+    }
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:8'
+        ]);
+
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => $request->password
+        ]);
+
+        return response()->json(['message' => 'Usuário criado com sucesso', 201]);
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (!$token = auth()->attempt($credentials)) {
+            return response()->json(['error' => 'Credenciais inválidas'], 401);
+        }
+
+        return response()->json([
+            'token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60,
+        ]);
+    }
+
+    public function refreshToken(Request $request)
+    {
+        return response()->json([
+            'token' => auth()->refresh(),
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60,
+        ]);
+    }
+}
