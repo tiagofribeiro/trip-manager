8611f2c3f6a9b4ca72c138ea10915a15ca216c8b
Tiago Ribeiro <tg.frnds@outlook.com>
2025-07-09 00:23:02 -0300

    :broom: cleanup & update

--- trip-api/config/database.php
+++ trip-api/config/database.php
@@ -11,7 +11,7 @@ return [
         'sqlite' => [
             'driver' => 'sqlite',
             'url' => env('DB_URL'),
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            'database' => env('DB_DATABASE', database_path('trip-manager.sqlite')),
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
             'busy_timeout' => null,
